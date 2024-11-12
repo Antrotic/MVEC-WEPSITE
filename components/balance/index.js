edfaabc93b087465ffd472bd8506ca1d6415c544
Jaser Deli <jaser11239@gmail.com>
2024-11-12 03:07:08 -0500

    Last Update

--- components/balance/index.js
+++ components/balance/index.js
@@ -1,35 +1,35 @@
-import { parseCookies } from "nookies";
-import React, { useContext } from "react";
-import { toast } from "react-toastify";
-import { Spinner } from "reactstrap";
-import ShoppingBag3FillIcon from "remixicon-react/ShoppingBag3FillIcon";
-import { DrawerConfig } from "../../configs/drawer-config";
-import { MainContext } from "../../context/MainContext";
-import { OrderContext } from "../../context/OrderContext";
-import { getPrice } from "../../utils/getPrice";
+import {parseCookies} from 'nookies';
+import React, {useContext} from 'react';
+import {toast} from 'react-toastify';
+import {Spinner} from 'reactstrap';
+import ShoppingBag3FillIcon from 'remixicon-react/ShoppingBag3FillIcon';
+import {DrawerConfig} from '../../configs/drawer-config';
+import {MainContext} from '../../context/MainContext';
+import {OrderContext} from '../../context/OrderContext';
+import {getPrice} from '../../utils/getPrice';
 
-const getBalanceClassNames = (displayPrice) => {
+const getBalanceClassNames = displayPrice => {
   const priceLength = displayPrice.toString().length;
-  const classNames = ["balance"];
-  if (priceLength > 11) classNames.push("balance--md");
-  if (priceLength > 16) classNames.push("balance--sm");
-  if (priceLength > 20) classNames.push("balance--xs");
+  const classNames = ['balance'];
+  if (priceLength > 11) classNames.push('balance--md');
+  if (priceLength > 16) classNames.push('balance--sm');
+  if (priceLength > 20) classNames.push('balance--xs');
 
-  return classNames.join(" ");
+  return classNames.join(' ');
 };
 
 function Balance() {
   const dc = DrawerConfig;
   const cookies = parseCookies();
-  const { handleVisible, handleAuth } = useContext(MainContext);
-  const { cartLoader, orderedProduct } = useContext(OrderContext);
+  const {handleVisible, handleAuth} = useContext(MainContext);
+  const {cartLoader, orderedProduct} = useContext(OrderContext);
 
   const displayPrice = getPrice(orderedProduct?.total_price);
   const onClick = () => {
-    if (cookies.access_token || cookies.cart_id) handleVisible(dc.order_list);
-    else {
-      toast.error("Please login first");
-      handleAuth("login");
+    if (cookies.cart_id || orderedProduct) {
+      handleVisible(dc.order_list);
+    } else {
+      toast.error('Please add items to your cart first');
     }
   };
   return (
